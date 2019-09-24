00e99e6962a172101a9201ad0985380d4d36ec50
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-09-24 00:01:14 -0300

    listando produtos

--- huntweb/src/pages/main/main.js
+++ huntweb/src/pages/main/main.js
@@ -3,17 +3,28 @@ import api from '../../services/api';
 
 export default class Main extends Component {
 
+   state = {
+      products: []
+   };
+
    componentDidMount(){
       this.loadProdcts();
    }
 
    loadProdcts = async () => {
       const response = await api.get('products');
-      console.log(response.data.docs);
+      this.setState({products: response.data.docs });
    };
 
    render(){
-     return <h1>Agora vai</h1>;
+     return (
+         <div className="products-list">
+            {this.state.products.map(product =>(
+               <h2 key={product._id}>{product.title}</h2>
+            ))}
+
+         </div>
+      );
    }  
     
 }
